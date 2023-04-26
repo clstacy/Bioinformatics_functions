644063b11112e87b568caa779edea36ed42be203
Carson Stacy <clstacy@uark.edu>
2023-04-26 12:32:39 -0500

    Update blast2go_gene_list_merger.R
    Add stringr import and make column names a parameter

--- blast2go_gene_list_merger.R
+++ blast2go_gene_list_merger.R
@@ -6,6 +6,7 @@
 #' Gene Ontology (GO) terms for each gene.
 #'
 #' @param blast2go_file Path to the Blast2GO output file.
+#' @param blast2go_columns Column names of the Blast2GO file as a character vector.
 #' @param gene_list_file Path to the gene list file.
 #' @param merged_file Path to the file to save the merged data to.
 #'
@@ -17,22 +18,22 @@
 #' @importFrom tidyr unite
 #' @importFrom tidyr pivot_wider
 #' @importFrom tidyr unnest
+#' @importFrom stringr str_remove
 #'
 #' @author Carson Stacy <carsonstacy2@gmail.com>
 #' @export
 #'
 #' @examples
-#' merge_blast2go_gene_list("blast2go_output.txt", "gene_list.txt", "merged_data.tsv")
+#' merge_blast2go_gene_list("blast2go_output.txt", c("Gene_ID", "Sequence_Length", "Sequence_Name", "E_Value", "Hit_ID", "Hit_Name", "Hit_Description", "GO"), "gene_list.txt", "merged_data.txt")
 #'
 #' @seealso https://www.blast2go.com/
 #' @seealso https://en.wikipedia.org/wiki/Gene_ontology
-merge_blast2go_gene_list <- function(blast2go_file, gene_list_file, merged_file) {
+merge_blast2go_gene_list <- function(blast2go_file, blast2go_columns, gene_list_file, merged_file) {
   # Read in the Blast2GO output file without column names
   blast2go <- read.delim(blast2go_file, header = FALSE, sep = "\t")
   
   # Add column names to the Blast2GO data frame
-  colnames(blast2go) <- c("Gene_ID", "Sequence_Length", "Sequence_Name", "E_Value", "Hit_ID", "Hit_Name", 
-"Hit_Description", "GO")
+  colnames(blast2go) <- blast2go_columns
   
   # Remove any non-alphanumeric endings from the gene IDs in the Blast2GO output file
   blast2go$Gene_ID <- stringr::str_remove(blast2go$Gene_ID, "\\W+$")
